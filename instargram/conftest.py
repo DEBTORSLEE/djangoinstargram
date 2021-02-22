d4c95c95802239d950cbbf4539733dec80d45e4a
debtolee <debtolee@gmail.com>
2021-02-22 12:02:43 +0900

    first commit

---
+++ instargram/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from instargram.users.models import User
+from instargram.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
